d3d4f7ea34dd1d983fc72b615dfd830abc456d23
yangshiwei <swearer23@163.com>
2023-09-22 21:46:57 +0800

    display default wod

---
+++ src/app/components/whiteboard.tsx
@@ -0,0 +1,57 @@
+'use client'
+import { useState } from 'react'
+import { Wod } from '@/types'
+import {BsBookmarkStar} from 'react-icons/bs'
+
+export default function WhiteBoard() {
+
+  const [wod, setWod] = useState(null as unknown as Wod)
+  const [loading, setLoading] = useState(false)
+
+  const generateWod = async () => {
+    setLoading(true)
+    const response = await fetch('/api/wod', {
+      method: 'POST',
+      body: JSON.stringify({})
+    })
+    const data = await response.json() as Wod
+    setWod(data)
+    setLoading(false)
+  }
+
+  if (!wod && !loading) generateWod()
+
+  if (loading) {
+    return (
+      <div className="mt-10">
+        <span className="loading loading-ring loading-lg"></span> 
+      </div>
+    )
+  } else if(wod) {
+    return (
+      <div className="mt-10 w-full prose">
+        <h1>
+          <BsBookmarkStar className="inline-block mr-2" />
+          {wod.title}
+        </h1>
+        <p>{wod.description}</p>
+        <section className='gap-4 grid grid-flow-col justify-start'>
+          <div className="badge badge-neutral">{wod.type}</div>
+          <div className="badge badge-neutral">{wod.timecap}</div>
+          {wod.round && <div className="badge badge-neutral">{wod.round}</div>}
+        </section>
+        <section>
+          <ul>
+            {wod.exercises.map((exercise, index) => {
+              return (
+                <li key={index}>
+                  <p>{exercise}</p>
+                </li>
+              )
+            })}
+          </ul>
+        </section>
+      </div>
+    )
+  }
+}
