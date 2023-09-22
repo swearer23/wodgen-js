85ce292eb8bcfdaffff6e9b9536d76891dc37c4f
yangshiwei <swearer23@163.com>
2023-09-22 22:27:43 +0800

    fetch new wod only on client side

--- src/app/components/whiteboard.tsx
+++ src/app/components/whiteboard.tsx
@@ -1,10 +1,9 @@
 'use client'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Wod } from '@/types'
 import {BsBookmarkStar} from 'react-icons/bs'
 
 export default function WhiteBoard() {
-
   const [wod, setWod] = useState(null as unknown as Wod)
   const [loading, setLoading] = useState(false)
 
@@ -19,8 +18,10 @@ export default function WhiteBoard() {
     setLoading(false)
   }
 
-  if (!wod && !loading) generateWod()
-
+  useEffect(() => {
+    if (!wod && !loading) generateWod()
+  }, [])
+  
   if (loading) {
     return (
       <div className="mt-10">
