7c105d70126e3aa62381b37955e793ec3e459d6d
yangshiwei <swearer23@163.com>
2023-10-24 15:41:58 +0800

    probe env var

--- src/const.ts
+++ src/const.ts
@@ -17,6 +17,12 @@ export enum WOD_PREFERENCE {
   // PR = 'PR',
 }
 
-const KV_PREFIX = process.env.NODE_ENV === 'production' ? 'PROD' : 'DEV'
+const ENV_PREFIX_MAP: {[key: string]: string} = {
+  production: 'PROD',
+  preview: 'PREVIEW',
+  development: 'DEV',
+}
+const VERCEL_ENV = process.env.VERCEL_ENV || 'development'
+const KV_PREFIX = ENV_PREFIX_MAP[VERCEL_ENV]
 export const SUBSCRIPTION_RDS_KEY = `${KV_PREFIX}_SUBSCRIPTIONS`
 export const DAILY_WOD_RDS_KEY = `${KV_PREFIX}_DAILY_WOD`
